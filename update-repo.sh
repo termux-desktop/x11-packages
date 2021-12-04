76af00c4105c22a788946bed6b71957d47f0f19f
Maxython <mixython@gmail.com>
2021-12-04 18:25:42 +0300

    Update update-repo.sh

--- update-repo.sh
+++ update-repo.sh
@@ -8,6 +8,17 @@ commet() {
 	echo -e "\033[0;32m-> $1\033[0m"
 }
 
+list_pkg() {
+	local list
+	for i in $(git status -s packages | awk '{print $2}'); do
+		local dir_sp=(${i//// })
+		if [[ ! $(echo $list | grep "${dir_sp[1]} ") ]]; then
+			list+="packages/${dir_sp[1]} "
+		fi
+	done
+	echo $list
+}
+
 # Update repo
 info "Update repo."
 git config pull.rebase false
@@ -26,36 +37,24 @@ done
 read -p "Username: " username
 read -sp "Key: " key
 echo
-git remote set-url origin "https://${username}:${key}@github.com/pkgs-termux/x11-packages.git"
+git remote set-url origin "https://${username}:${key}@github.com/Maxython/termux-packages.git"
 git reset
 
 # Edit file
 for i in $(grep -l ">>>>>>>" packages/*/*); do
 	rm $i
-	wget -O $i https://raw.githubusercontent.com/termux/x11-packages/master/$i
+	wget -O $i https://raw.githubusercontent.com/termux/termux-packages/master/$i
 done
 
 # Sort file
 info "Sort and push packages."
-for i in $(git status packages/ | grep "packages/" | sed "s|modified:||g; s|new file:||g; s|deleted:||g; s|both||g; s|added:||g"); do
-	file_sp=(${i//// })
-	name=${file_sp[1]}
-	commet "Package ${name}: ${i}."
-	if [[ -n $list_name ]] && [[ ! $(echo $list_name | grep $name) ]]; then
-		commet "Push package ${old_name}."
-		git commit -m "Update package ${old_name}"
-		git push origin master
-	else
-		git reset packages/${old_name}
-	fi
-	git add packages/${name}
-	list_name+="$name "
-	old_name="$name"
+for i in $(list_pkg); do
+	commet "${i}"
+	git add $i
+	git commit -m "${i}"
+	git push origin master
 done
-commet "Push package ${old_name}."
-git commit -m "Update package ${old_name}"
-git push origin master
 
-git remote set-url origin "https://github.com/pkgs-termux/x11-packages"
+git remote set-url origin "https://github.com/Maxython/termux-packages"
 
 info "Done."
