ef585a99c49ecc5d7c92f278d27466bd54ebb812
Maxython <mixython@gmail.com>
2021-12-26 00:10:35 +0300

    packages/xfce4-calculator-plugin

--- packages/xfce4-calculator-plugin/build.sh
+++ packages/xfce4-calculator-plugin/build.sh
@@ -6,3 +6,4 @@ TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-calculator-plugin/${TERMUX_PKG_VERSION:0:3}/xfce4-calculator-plugin-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=e4016a03c3ef4ebddd97e4135f5e304f80677033c98e19644b9989ec6f5ada81
 TERMUX_PKG_DEPENDS="libxfce4ui, libxfce4util, xfce4-panel, gtk3"
+
