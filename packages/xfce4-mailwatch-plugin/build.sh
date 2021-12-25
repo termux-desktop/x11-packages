bad60f17ddb0e71dd66bddb731012bb3434a555a
Maxython <mixython@gmail.com>
2021-12-26 00:10:43 +0300

    packages/xfce4-mailwatch-plugin

--- packages/xfce4-mailwatch-plugin/build.sh
+++ packages/xfce4-mailwatch-plugin/build.sh
@@ -6,3 +6,4 @@ TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-mailwatch-plugin/${TERMUX_PKG_VERSION:0:3}/xfce4-mailwatch-plugin-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=20f91ebefd2880b27f421f773115b3740f67de2bf60feace3841bfd1a09cbe2e
 TERMUX_PKG_DEPENDS="glib, libxfce4ui, libxfce4util, xfce4-panel"
+
