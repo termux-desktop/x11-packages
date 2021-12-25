49ed20d3fc8f12be7289d9643343b72fea020d9e
Maxython <mixython@gmail.com>
2021-12-26 00:10:53 +0300

    packages/xfce4-taskmanager

--- packages/xfce4-taskmanager/build.sh
+++ packages/xfce4-taskmanager/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-taskmanager/1.5/xfce4-taskmanager-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=bd25143f47a29000b4148874863dffa521b1a37cb01dbc026f423ea3160f9a35
 TERMUX_PKG_DEPENDS="xfce4-panel, libxmu"
+
